1aafd9e21c74de02356ff3ebeefbc4b1397f3aa2
Alan Oliveira <alan_tva_90@yahoo.com.br>
2020-08-17 17:12:26 -0300

    Updated Login Page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,49 +1,5 @@
-import styled from 'styled-components'
-
-import MuiContainer from '@material-ui/core/Container'
-
-import Logo from '../components/atoms/Logo'
 import FormLogin from '../components/templates/FormLogin'
-import LinkSignUp from '../components/templates/LinkSignUp'
 
-const ContainerLogin = () => (
-  <MuiContainer component="main" maxWidth="xs">
-    <Wrapper>
-      <LogoWrapper>
-        <Logo />
-      </LogoWrapper>
-      <LoginWrapper>
-        <FormLogin />
-      </LoginWrapper>
-      <LinkSignUpWrapper>
-        <LinkSignUp />
-      </LinkSignUpWrapper>
-    </Wrapper>
-  </MuiContainer>
-)
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  min-height: 100vh;
-  max-width: 400px;
-`
-
-const LogoWrapper = styled.div`
-  max-width: 220px;
-  margin: 10px 0;
-`
-
-const LoginWrapper = styled.div`
-  width: 100%;
-  margin-bottom: 10px;
-`
-
-const LinkSignUpWrapper = styled.div`
-  text-align: center;
-  margin-bottom: 10px;
-`
+const ContainerLogin = () => <FormLogin />
 
 export default ContainerLogin
